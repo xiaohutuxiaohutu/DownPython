804c779fb5c4093e4668641bc84e718690170dc4
xioahutu <1328669873@qq.com>
2019-09-22 16:22:14 +0800

    update 0922

--- common/__init__.py
+++ common/__init__.py
@@ -342,10 +342,10 @@ def down_all_pic(cur_dir, replace_url, down_file_path):
                         fileUrl = file_url.replace('http://pic.w26.rocks/', replace_url)
                         image_name = fileUrl.split("/")[-1]
                         if not os.path.exists(image_name):
-                            print('下载第' + total + '-' + str(i + 1) + '个:' + file_url)
+                            print('第' + str(num) + '行：第 ' + str(i + 1) + ' / ' + total + '个:' + file_url)
                             down_img(fileUrl)
                         else:
-                            print('第' + total + '-' + str(i + 1) + '个已存在:' + file_url)
+                            print('第' + str(num) + '行：第 ' + str(i + 1) + ' / ' + total + '个:' + file_url)
                 print("-----down over----------------")
         os.remove(file_name)
     print("all over")
