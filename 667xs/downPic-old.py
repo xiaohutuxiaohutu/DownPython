545ebf4d6b2c0d23775d3c10b2908449c85b9940
xioahutu <1328669873@qq.com>
2019-10-07 17:04:10 +0800

    PPP91 update 20191007

--- 667xs/downPic-old.py
+++ 667xs/downPic-old.py
@@ -21,43 +21,45 @@ sys.path.append(rootDir)
 
 down_path = 'D:/图片/667xs/自拍偷拍/' + datetime.datetime.now().strftime('%Y-%m-%d') + '/'
 proxy_ip = common.get_ip()
-file_path = curDir + '/667xs_2019-02-24_0.txt'
-file = open(file_path)
-# 获取总行数
-for num, value in enumerate(file, 1):
-    line = value.strip('\n')
-    print('第' + str(num) + '行： ' + line)
-    html = requests.get(line, headers=header, proxies=proxy_ip)
-    html.encoding = 'gb2312'
-    itemSoup = BeautifulSoup(html.text, "lxml")
-    title = itemSoup.title.string
-    posi = title.index('_自拍偷拍')
-    title = title[0:posi]
-    new_title = common.replace_special_char(title).strip()
-    print(new_title)
-    # print(title)
-    imgUrls = itemSoup.select(
-        "body div[id='wrap'] div[id='ks'] div[id='ks_xp'] div[class='main'] div[class='content'] div div[class='n_bd'] img")
-    print('图片数量：' + str(len(imgUrls)))
-    path = down_path + str(new_title) + '/'
-    s = len(imgUrls)
-    if len(imgUrls) > 1:
-        for i in range(0, len(imgUrls)):
-            img_url = imgUrls[i].get('src')
+file_path = curDir + '/667xs_2019-02-24_1.txt'
+
+
+def down_pic(img_urls, down_file_path, row_num):
+    s = len(img_urls)
+    if len(img_urls) > 1:
+        for i in range(0, len(img_urls)):
+            img_url = img_urls[i].get('src')
             if img_url.startswith('http://tu.2015img.com'):
                 image_name = img_url.split("/")[-1]
-
-                print('下载第' + str(num) + '行； 第' + str(i + 1) + '  / ' + str(s) + '  个: ' + img_url)
+                print('下载第' + str(row_num) + '行； 第' + str(i + 1) + '  / ' + str(s) + '  个: ' + img_url)
                 imageUrl = requests.get(img_url, headers=header)
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                os.chdir(path)
+                if not os.path.exists(down_file_path):
+                    os.makedirs(down_file_path)
+                os.chdir(down_file_path)
                 if not os.path.exists(image_name):
-                    f = open(image_name, 'wb')
-                    f.write(imageUrl.content)
-                    f.close()
+                    with open(image_name, 'wb') as f:
+                        f.write(imageUrl.content)
     print("-----down over----------------")
 
-file.close
+
+# 获取总行数
+with open(file_path) as file:
+    for num, value in enumerate(file, 1):
+        line = value.strip('\n')
+        print('第' + str(num) + '行： ' + line)
+        html = requests.get(line, headers=header, proxies=proxy_ip)
+        html.encoding = 'gb2312'
+        itemSoup = BeautifulSoup(html.text, "lxml")
+        title = itemSoup.title.string
+        posi = title.index('_自拍偷拍')
+        title = title[0:posi]
+        new_title = common.replace_special_char(title).strip()
+        print(new_title)
+        # print(title)
+        imgUrls = itemSoup.select(
+            "body div[id='wrap'] div[id='ks'] div[id='ks_xp'] div[class='main'] div[class='content'] div div[class='n_bd'] img")
+        print('图片数量：' + str(len(imgUrls)))
+        path = down_path + str(new_title) + '/'
+        down_pic(imgUrls, path, num)
 os.remove(file_path)
 print("all over")
--- ppp91/downpic-ppp91.py
+++ ppp91/downpic-ppp91.py
@@ -19,51 +19,59 @@ header = {
 ISOTIMEFORMAT = '%Y-%m-%d %X'
 down_path = 'D:/图片/PPP91/' + datetime.datetime.now().strftime('%Y-%m-%d') + '/'
 file_path = curDir + '/zipai_2018-09-01_0.txt'
-file = open(file_path)
-# 获取总行数
-for num, value in enumerate(file, 1):
-    # print('第'+str(num)+'行：')
-    line = value.strip('\n')
-    print('第%i行:%s' % (num, line))
-    # 获取代理服务器
-    proxy_ip = common.get_ip()
-    try:
-        html = requests.get(line, headers=header, proxies=proxy_ip, timeout=10)
-        html.encoding = 'utf-8'
-        itemSoup = BeautifulSoup(html.text, 'lxml')
-        title = itemSoup.title.string
-        title = common.replace_special_char(title)
-        ind = title.index('-')
-        newTitle = title[0:ind]
-        imgUrls = itemSoup.select("body div[class='main'] div[class='contentList'] div[class='content'] p img")
 
-        print('name：%s；图片个数：%s' % (str(newTitle.strip()), str(len(imgUrls))))
-        total_num = len(imgUrls)
-        if total_num > 1:
+
+def down_pic(img_urls, down_file_path, row_num):
+    total_num = len(img_urls)
+    if total_num > 1:
+        # path = down_path + str(newTitle.strip()) + '/'
+        if not os.path.exists(down_file_path):
+            os.makedirs(down_file_path)
+        os.chdir(down_file_path)
+        for i in range(0, total_num):
+            img_url = img_urls[i].get('src')
+            image_name = img_url.split("/")[-1]
+
+            try:
+                imageUrl = requests.get(img_url, headers=header, proxies=proxy_ip, timeout=15)
+                if imageUrl.status_code == 200:
+                    if not (os.path.exists(path)):
+                        os.makedirs(path)
+                    os.chdir(path)
+                    if not os.path.exists(image_name):
+                        print('下载第' + str(row_num) + '行； 第' + str(i + 1) + '  / ' + str(total_num) + '  个: ' + img_url)
+                        with open(image_name, 'wb') as f:
+                            f.write(imageUrl.content)
+                    else:
+                        print('第' + str(row_num) + '行； 第' + str(i + 1) + '  / ' + str(total_num) + '  个 已存在: ' + img_url)
+            except requests.exceptions.RequestException:
+                # restart+=1
+                print('--第%i：---%s连接错误----' % (i + 1, img_url))
+                # print('尝试第%i次连接'%restart)
+        print("---------------------")
+
+
+with open(file_path) as file:
+    for num, value in enumerate(file, 1):
+        line = value.strip('\n')
+        print('第%i行:%s' % (num, line))
+        # 获取代理服务器
+        proxy_ip = common.get_ip()
+        try:
+            html = requests.get(line, headers=header, proxies=proxy_ip, timeout=10)
+            html.encoding = 'utf-8'
+            itemSoup = BeautifulSoup(html.text, 'lxml')
+            title = itemSoup.title.string
+            title = common.replace_special_char(title)
+            ind = title.index('-')
+            newTitle = title[0:ind]
+            imgUrls = itemSoup.select("body div[class='main'] div[class='contentList'] div[class='content'] p img")
+
+            print('name：%s；图片个数：%s' % (str(newTitle.strip()), str(len(imgUrls))))
             path = down_path + str(newTitle.strip()) + '/'
-            if not os.path.exists(path):
-                os.makedirs(path)
-            os.chdir(path)
-            for i in range(0, total_num):
-                img_url = imgUrls[i].get('src')
-                image_name = img_url.split("/")[-1]
-                print('下载第' + str(num) + '行； 第' + str(i + 1) + '  / ' + str(total_num) + '  个: ' + img_url)
-                try:
-                    imageUrl = requests.get(img_url, headers=header, proxies=proxy_ip, timeout=15)
-                    if imageUrl.status_code == 200:
-                        if not (os.path.exists(path)):
-                            os.makedirs(path)
-                        os.chdir(path)
-                        f = open(image_name, 'wb')
-                        f.write(imageUrl.content)
-                        f.close()
-                except requests.exceptions.RequestException:
-                    # restart+=1
-                    print('--第%i：---%s连接错误----' % (i + 1, img_url))
-                    # print('尝试第%i次连接'%restart)
-            print("---------------------")
-    except requests.exceptions.RequestException:
-        print('第%i行:%s---连接超时' % (num, line))
-file.close
+            down_pic(imgUrls, path, num)
+        except requests.exceptions.RequestException:
+            print('第%i行:%s---连接超时' % (num, line))
+
 os.remove(file_path)
 print("all over")
