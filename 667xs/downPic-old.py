e87a7eeb601513dab080067c609b97c7a8e35342
xioahutu <1328669873@qq.com>
2019-10-07 17:25:28 +0800

    PPP91 update 20191007

--- 667xs/downPic-old.py
+++ 667xs/downPic-old.py
@@ -32,13 +32,16 @@ def down_pic(img_urls, down_file_path, row_num):
             if img_url.startswith('http://tu.2015img.com'):
                 image_name = img_url.split("/")[-1]
                 print('下载第' + str(row_num) + '行； 第' + str(i + 1) + '  / ' + str(s) + '  个: ' + img_url)
-                imageUrl = requests.get(img_url, headers=header)
-                if not os.path.exists(down_file_path):
-                    os.makedirs(down_file_path)
-                os.chdir(down_file_path)
-                if not os.path.exists(image_name):
-                    with open(image_name, 'wb') as f:
-                        f.write(imageUrl.content)
+                try:
+                    imageUrl = requests.get(img_url, headers=header)
+                    if not os.path.exists(down_file_path):
+                        os.makedirs(down_file_path)
+                    os.chdir(down_file_path)
+                    if not os.path.exists(image_name):
+                        with open(image_name, 'wb') as f:
+                            f.write(imageUrl.content)
+                except requests.exceptions.RequestException:
+                    print('第%i行:%s---连接超时' % (row_num, img_url))
     print("-----down over----------------")
 
 
@@ -47,19 +50,22 @@ with open(file_path) as file:
     for num, value in enumerate(file, 1):
         line = value.strip('\n')
         print('第' + str(num) + '行： ' + line)
-        html = requests.get(line, headers=header, proxies=proxy_ip)
-        html.encoding = 'gb2312'
-        itemSoup = BeautifulSoup(html.text, "lxml")
-        title = itemSoup.title.string
-        posi = title.index('_自拍偷拍')
-        title = title[0:posi]
-        new_title = common.replace_special_char(title).strip()
-        print(new_title)
-        # print(title)
-        imgUrls = itemSoup.select(
-            "body div[id='wrap'] div[id='ks'] div[id='ks_xp'] div[class='main'] div[class='content'] div div[class='n_bd'] img")
-        print('图片数量：' + str(len(imgUrls)))
-        path = down_path + str(new_title) + '/'
-        down_pic(imgUrls, path, num)
+        try:
+            html = requests.get(line, headers=header, proxies=proxy_ip)
+            html.encoding = 'gb2312'
+            itemSoup = BeautifulSoup(html.text, "lxml")
+            title = itemSoup.title.string
+            posi = title.index('_自拍偷拍')
+            title = title[0:posi]
+            new_title = common.replace_special_char(title).strip()
+            print(new_title)
+            # print(title)
+            imgUrls = itemSoup.select(
+                "body div[id='wrap'] div[id='ks'] div[id='ks_xp'] div[class='main'] div[class='content'] div div[class='n_bd'] img")
+            print('图片数量：' + str(len(imgUrls)))
+            path = down_path + str(new_title) + '/'
+            down_pic(imgUrls, path, num)
+        except requests.exceptions.RequestException:
+            print('第%i行:%s---连接超时' % (num, line))
 os.remove(file_path)
 print("all over")
