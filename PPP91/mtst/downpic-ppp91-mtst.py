59317c392f19b4b28794ae3a0b5222a1be975b9a
xioahutu <1328669873@qq.com>
2019-10-08 14:25:09 +0800

    porn update 20191008 09:25

--- PPP91/mtst/downpic-ppp91-mtst.py
+++ PPP91/mtst/downpic-ppp91-mtst.py
@@ -4,7 +4,10 @@ import PPP91
 import threading
 
 curDir = os.path.abspath(os.curdir)  # 当前文件路径
-down_path = 'D:/图片/PPP91/美腿/%s/' % (datetime.datetime.now().strftime('%Y-%m-%d'))
+# down_path = 'D:/图片/PPP91/美腿/%s/' % (datetime.datetime.now().strftime('%Y-%m-%d'))
+userPath = os.path.expanduser('~')  # 获取用户目录
+# 文件下载保存路径
+down_path = userPath + '/Pictures/Camera Roll/PPP91/meitui/'
 down_param = {
     'cur_dir': curDir,
     'down_path': down_path
--- PPP91/wyzp/downpic-ppp91-wyzp.py
+++ PPP91/wyzp/downpic-ppp91-wyzp.py
@@ -3,9 +3,10 @@ import os
 import PPP91
 
 curDir = os.path.abspath(os.curdir)  # 当前文件路径
-ISOTIMEFORMAT = '%Y-%m-%d %X'
-# down_path = 'D:/图片/PPP91/网友自拍/' + datetime.datetime.now().strftime('%Y-%m-%d') + '/'
-down_path = 'D:/图片/PPP91/网友自拍/%s/' % (datetime.datetime.now().strftime('%Y-%m-%d'))
+# down_path = 'D:/图片/PPP91/网友自拍/%s/' % (datetime.datetime.now().strftime('%Y-%m-%d'))
+userPath = os.path.expanduser('~')  # 获取用户目录
+# 文件下载保存路径
+down_path = userPath + '/Pictures/Camera Roll/PPP91/meitui/'
 down_param = {
     'cur_dir': curDir,
     'down_path': down_path
