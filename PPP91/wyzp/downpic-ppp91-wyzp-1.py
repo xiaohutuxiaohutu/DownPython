dcb48b37bffcf7078f51f7074fc4a8c03b00cae0
xioahutu <1328669873@qq.com>
2019-10-16 09:07:42 +0800

    update

---
+++ PPP91/wyzp/downpic-ppp91-wyzp-1.py
@@ -0,0 +1,19 @@
+import datetime
+import os
+import PPP91
+
+curDir = os.path.abspath(os.curdir)  # 当前文件路径
+# down_path = 'D:/图片/PPP91/网友自拍/%s/' % (datetime.datetime.now().strftime('%Y-%m-%d'))
+userPath = os.path.expanduser('~')  # 获取用户目录
+# 文件下载保存路径
+down_path = userPath + '/Pictures/Camera Roll/PPP91/wyzp/'
+file_name = curDir + '/2019-10-07_6.txt'
+with open(file_name) as file:
+    for num, value in enumerate(file, 1):
+        line = value.strip('\n')
+        print('第%i行:%s' % (num, line))
+        img_urls = PPP91.get_img_urls(line, num)
+        path = down_path + img_urls[0] + '/'
+        PPP91.down_pic(img_urls[1], path, num)
+os.remove(file_name)
+
