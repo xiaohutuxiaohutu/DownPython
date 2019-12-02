e3b641a62362a2c3136b3b75139f9af152971173
xioahutu <1328669873@qq.com>
2019-12-02 17:30:00 +0800

    modify name

---
+++ porn/write.sh
@@ -0,0 +1,5 @@
+./jh/zpdr_ycsq_jh/WriteToTxt-JH-ZPDRYCSQ.py
+./jh/zpdr_ycsq_jh/WriteToTxt-JH-woaiwoqi.py
+./all/xqfx/writeToTxt-all-xqfx.py
+./all/zpdr_ycsq_all/WriteToTxt-All-ZPDRYCSQ.py
+./all/wawq_all/WriteToTxt-All-WAWQ.py
