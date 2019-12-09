d1c4156c003729a24fa90413f6e1a072bc17195c
xioahutu <1328669873@qq.com>
2019-12-09 09:19:01 +0800

    update 1209

--- porn/all/wawq_all/Down-All-WAWQ.py
+++ porn/all/wawq_all/Down-All-WAWQ.py
@@ -9,7 +9,7 @@ curDir = os.path.abspath(os.curdir) + os.sep
 userPath = os.path.expanduser('~') + os.sep  # 获取用户目录、
 # downFilePath = userPath + 'Pictures/Camera Roll/all/woaiwoqi/'
 downFilePath = 'D:/图片/91porn/ALL/我爱我妻/' + (datetime.datetime.now().strftime('%Y-%m')) + '/'
-# downFilePath = curDir + (datetime.datetime.now().strftime('%Y-%m-%d')) + os.sep
+# downFilePath = curDir + (datetime.datetime.now().strftime('%Y-%m')) + os.sep
 down_param = {
     'cur_dir': curDir,
     'down_file_path': downFilePath
--- porn/all/xqfx/downPic-all-xqfx.py
+++ porn/all/xqfx/downPic-all-xqfx.py
@@ -9,7 +9,7 @@ curDir = os.path.abspath(os.curdir) + os.sep
 userPath = os.path.expanduser('~') + os.sep
 # downFilePath = userPath + 'Pictures/Camera Roll/all/xingqufenxiang/'
 downFilePath = 'D:/图片/91porn/ALL/兴趣分享/' + (datetime.datetime.now().strftime('%Y-%m')) + '/'
-# downFilePath = curDir + (datetime.datetime.now().strftime('%Y-%m-%d')) + os.sep
+# downFilePath = curDir + (datetime.datetime.now().strftime('%Y-%m')) + os.sep
 down_param = {
     'cur_dir': curDir,
     'down_file_path': downFilePath
