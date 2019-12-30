cb8690dba060de881ed0f94af2eb603c537e80d0
xioahutu <1328669873@qq.com>
2019-12-30 09:54:51 +0800

    update 191230

--- common/__init__.py
+++ common/__init__.py
@@ -163,7 +163,7 @@ def down_img(file_url):
         get_request = requests.get(file_url, headers=header, proxies=proxy_ip)
         image = get_request.content
         image_b = io.BytesIO(image).read()
-        print('image size %i kb：' % (len(image_b) / 1000))
+        print('image size : %i kb' % (len(image_b) / 1000))
         with open(image_name, 'wb') as f:
             f.write(image)
     # else:
