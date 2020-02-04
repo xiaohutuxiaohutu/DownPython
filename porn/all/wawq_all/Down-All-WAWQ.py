86599f2b6d9acfa987fcac0555dab58bbd5ae85f
xioahutu <1328669873@qq.com>
2020-02-04 14:25:31 +0800

    update 200204

--- porn/all/wawq_all/Down-All-WAWQ.py
+++ porn/all/wawq_all/Down-All-WAWQ.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 import os
-import sys
-
-curDir = os.path.abspath(os.curdir)  # 获取当前文件路径
-rootDir = curDir[:curDir.find("DownPython\\") + len("DownPython\\")]  # 获取myProject，也就是项目的根路径
-# sys.path.append(r"C:\workspace\GitHub\DownPython")
-sys.path.append(rootDir)
 
 import porn
 
+# curDir = os.path.abspath(os.curdir)  # 获取当前文件路径
+# rootDir = curDir[:curDir.find("DownPython\\") + len("DownPython\\")]  # 获取myProject，也就是项目的根路径
+# # sys.path.append(r"C:\workspace\GitHub\DownPython")
+# sys.path.append(rootDir)
+
 # 获取用户目录、
-# userPath = os.path.expanduser('~') + os.sep
-# downFilePath = userPath + 'Pictures/Camera Roll/all/woaiwoqi/'
-downFilePath = 'D:/图片/91porn/ALL/我爱我妻'
+userPath = os.path.expanduser('~') + os.sep
+downFilePath = userPath + 'Pictures/Camera Roll/all/woaiwoqi/'
+# downFilePath = 'F:/图片/91porn/ALL/我爱我妻'
 down_param = {
     'down_file_path': downFilePath
 }
