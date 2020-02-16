d3df43baceb8a3582470ff7c985cefc8c97c8771
小糊涂 <1328669873@qq.com>
2020-02-16 10:35:36 +0800

    update 0216

--- porn/jh/wawq_jh/Write-JH-wawq.py
+++ porn/jh/wawq_jh/Write-JH-wawq.py
@@ -2,7 +2,7 @@
 import porn
 
 down_param = {
-    'done_down_text': 'DoneDown-JH-WAWQ.text',
+    'done_down_text': 'Done-JH-WAWQ.text',
     'down_url': 'forumdisplay.php?fid=21&orderby=dateline&filter=digest&page=%i',
     'start_page': 1,
     'end_page': 2
