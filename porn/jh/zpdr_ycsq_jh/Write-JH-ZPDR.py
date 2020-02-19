6b6b7614928a5023df74d7b125a66143203cb5bd
xiaohutu <1328669873@qq.com>
2020-02-19 15:35:10 +0800

    update 20200219

--- porn/jh/zpdr_ycsq_jh/Write-JH-ZPDR.py
+++ porn/jh/zpdr_ycsq_jh/Write-JH-ZPDR.py
@@ -2,12 +2,13 @@
 # coding=UTF-8
 import os
 import sys
-print(sys.path)
-curDir = os.path.abspath(os.curdir)  # 获取当前文件路径
-rootDir = curDir[:curDir.find("DownPython\\") + len("DownPython\\")]  # 获取myProject，也就是项目的根路径
-print(curDir)
-print(rootDir)
-sys.path.append(rootDir)
+
+# print(sys.path)
+# curDir = os.path.abspath(os.curdir)  # 获取当前文件路径
+# rootDir = curDir[:curDir.find("DownPython\\") + len("DownPython\\")]  # 获取myProject，也就是项目的根路径
+# print(curDir)
+# print(rootDir)
+# sys.path.append(rootDir)
 #
 # sys.path.append(r"C:\workspace\GitHub\DownPython")
 
