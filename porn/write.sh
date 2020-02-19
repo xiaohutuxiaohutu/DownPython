4267cb42d34e4ea787d783d1bca339132854b159
xiaohutu <1328669873@qq.com>
2020-02-19 15:44:07 +0800

    update 20200219

--- porn/write.sh
+++ porn/write.sh
@@ -1,5 +1,5 @@
-./jh/zpdr_ycsq_jh/Write-JH-ZPDR.py
-./jh/zpdr_ycsq_jh/Write-JH-WAWQ.py
-./all/xqfx/Write-All-XQFX.py
-./all/zpdr_ycsq_all/Write-All-ZPDR.py
-./all/wawq_all/Write-All-WAWQ.py
+python3 ./jh/zpdr_ycsq_jh/Write-JH-ZPDR.py
+python3 ./jh/zpdr_ycsq_jh/Write-JH-WAWQ.py
+python3 ./all/xqfx/Write-All-XQFX.py
+python3 ./all/zpdr_ycsq_all/Write-All-ZPDR.py
+python3 ./all/wawq_all/Write-All-WAWQ.py
